c1c54db2c863b0d5f671a87d104568edd38bcf62
DiveInQuestion <1936004344@qq.com>
2019-04-05 15:53:14 +0800

    test

--- src/helloworld/hello.java
+++ src/helloworld/hello.java
@@ -3,6 +3,6 @@ package helloworld;
 public class hello {
 	public static void main(String args[]) {
 		System.out.println("I say Hello World again!");
-		System.out.println("Hi!");
+		System.out.println("Hi,my dear!");
 	}
 }
