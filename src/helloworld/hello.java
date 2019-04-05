f2e2a069dbd691e9559887f5c42b3c7b77515650
DiveInQuestion <1936004344@qq.com>
2019-04-05 18:39:31 +0800

    test

--- src/helloworld/hello.java
+++ src/helloworld/hello.java
@@ -3,11 +3,7 @@ package helloworld;
 public class hello {
 	public static void main(String args[]) {
 		System.out.println("I say Hello World again!");
-<<<<<<< HEAD
-		System.out.println("Hi,my dear!");
-		System.out.println("Hi!");
-=======
-		System.out.println("Hello!");
->>>>>>> refs/remotes/choose_remote_name/develop
+		System.out.println("My Dear!");
+		System.out.println("hi,I'm here!");
 	}
 }
