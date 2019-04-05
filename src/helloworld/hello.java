e7483e0b9b75ac306192d73a3f99c0e193ee4524
DiveInQuestion <1936004344@qq.com>
2019-04-05 11:16:11 +0800

    test

--- src/helloworld/hello.java
+++ src/helloworld/hello.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class hello {
 	public static void main(String args[]) {
-		System.out.println("Hello,Hello World!");
+		System.out.println("Now,Hello,Hello World!");
 	}
 }
