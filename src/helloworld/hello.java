afff4b598d0a0d2b5bc3a924bed55590698a9f90
DiveInQuestion <1936004344@qq.com>
2019-04-09 10:59:13 +0800

    test

--- src/helloworld/hello.java
+++ src/helloworld/hello.java
@@ -4,5 +4,7 @@ public class hello {
 	public static void main(String args[]) {
 		System.out.println("I say Hello World again!");
 		System.out.println("My Dear");
+		System.out.println("New!");
+		
 	}
 }
