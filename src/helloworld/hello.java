d2407f445a09ede4bc2d6e8189791e085aaec1d0
DiveInQuestion <1936004344@qq.com>
2019-04-09 11:07:44 +0800

    Update hello.java

--- src/helloworld/hello.java
+++ src/helloworld/hello.java
@@ -4,7 +4,6 @@ public class hello {
 	public static void main(String args[]) {
 		System.out.println("I say Hello World again!");
 		System.out.println("My Dear");
-		System.out.println("New!");
-		
+		System.out.println("Another one update");
 	}
 }
